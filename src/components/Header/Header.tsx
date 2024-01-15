5e4873459f219597cc084429f31b9be5c5321846
Metamotens <roberhdezmora88@gmail.com>
2024-01-15 16:35:04 +0100

    sidenav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { component$ } from "@builder.io/qwik";
+
 import Bell from "/icons/bell.svg";
 import Plus from "/icons/plus.svg";
 import Dots from "/icons/dots.svg";
@@ -8,7 +9,7 @@ export interface HeaderProps {}
 
 export const Header = component$<HeaderProps>(() => {
   return (
-    <header class="flex flew-row justify-between items-center w-full py-7">
+    <header class="flex flew-row justify-between items-center w-full pt-7 pb-8">
       <span class="text-lg tracking-[5px] font-black pr-7">RAWG</span>
       <div class="relative w-full pr-7">
         <div class="absolute inset-y-0 start-1 flex items-center ps-3 pointer-events-none">
@@ -17,11 +18,11 @@ export const Header = component$<HeaderProps>(() => {
         <input
           type="search"
           placeholder="Search 861,608 games"
-          class="block w-full p-3.5 ps-10 text-sm outline-none text-gray-900 rounded-full bg-[#3b3b3b] hover:bg-white focus:bg-white transition-colors duration-300"
+          class="block w-full p-3 ps-10 text-sm outline-none text-gray-900 rounded-full bg-[#3b3b3b] hover:bg-white focus:bg-white transition-colors duration-300"
         />
       </div>
       <div class="flex flew-row w-64 space-x-4 justify-end text-sm">
-        <span>My library</span>
+        <span class="cursor-pointer hover:underline">My library</span>
         <img
           src={Bell}
           alt="bell"
@@ -37,7 +38,13 @@ export const Header = component$<HeaderProps>(() => {
           class="cursor-pointer"
         />
         <span class="cursor-pointer hover:underline font-bold">API</span>
-        <img src={Dots} alt="dots" width={14} height={14} />
+        <img
+          class="cursor-pointer"
+          src={Dots}
+          alt="dots"
+          width={14}
+          height={14}
+        />
       </div>
     </header>
   );
--- src/components/Sidenav/Sidenav.tsx
+++ src/components/Sidenav/Sidenav.tsx
@@ -1,7 +1,278 @@
 import { component$ } from "@builder.io/qwik";
 
-export interface SidenavProps {}
+import Star from "/icons/star.svg";
+import Fire from "/icons/fire.svg";
+import Next from "/icons/next.svg";
+import Calendar from "/icons/calendar.svg";
+import Trophy from "/icons/trophy.svg";
+import Statistics from "/icons/statistics.svg";
+import Crown from "/icons/crown.svg";
+import Pad from "/icons/pad.svg";
+import Download from "/icons/download.svg";
+import Folder from "/icons/folder.svg";
+import Windows from "/icons/windows.svg";
+import Playstation from "/icons/playstation.svg";
+import Xbox from "/icons/xbox.svg";
 
-export const Sidenav = component$<SidenavProps>((props) => {
-  return <aside>Sidenav component works!</aside>;
+import Action from "/images/action.png";
+import Strategy from "/images/strategy.png";
+import Rpg from "/images/rpg.png";
+
+export const Sidenav = component$(() => {
+  return (
+    <nav class="flex flex-row w-[250px]">
+      <ul class="space-y-5 mt-11">
+        <li>
+          <a
+            href="/"
+            class="text-2xl block font-bold hover:text-[#FFFFFF66] transition-color transition-all"
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            href="/"
+            class="text-2xl block font-bold hover:text-[#FFFFFF66] transition-color transition-all"
+          >
+            Reviews
+          </a>
+        </li>
+        <li>
+          <span class="text-2xl block font-bold hover:text-[#FFFFFF66] transition-color transition-all">
+            New Releases
+          </span>
+          <ul class="flex flex-col gap-2 pt-3">
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Star} alt="Star" width={18} height={18} />
+                </span>
+                <span class="text-base">Last 30 days</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Fire} alt="Fire" width={18} height={18} />
+                </span>
+                <span class="text-base">This week</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Next} alt="Next" width={18} height={18} />
+                </span>
+                <span class="text-base">Next week</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Calendar} alt="calendar" width={18} height={18} />
+                </span>
+                <span class="text-base">Release calendar</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <span class="text-2xl block font-bold hover:text-[#FFFFFF66] transition-color transition-all">
+            Top
+          </span>
+          <ul class="flex flex-col gap-2 pt-3">
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Trophy} alt="Trophy" width={18} height={18} />
+                </span>
+                <span class="text-base">Best of the year</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img
+                    src={Statistics}
+                    alt="Statistics"
+                    width={18}
+                    height={18}
+                  />
+                </span>
+                <span class="text-base">Popular in 2023</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Crown} alt="Crown" width={18} height={18} />
+                </span>
+                <span class="text-base">All time top 250</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a
+            href="/"
+            class="text-2xl block font-bold hover:text-[#FFFFFF66] transition-color transition-all"
+          >
+            All Games
+          </a>
+        </li>
+        <li>
+          <a
+            href="/"
+            class="text-2xl block font-bold hover:text-[#FFFFFF66] transition-color transition-all"
+          >
+            Browse
+          </a>
+          <ul class="flex flex-col gap-2 pt-3">
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Pad} alt="Pad" width={18} height={18} />
+                </span>
+                <span class="text-base">Platforms</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Download} alt="Download" width={18} height={18} />
+                </span>
+                <span class="text-base">Stores</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Folder} alt="Folder" width={18} height={18} />
+                </span>
+                <span class="text-base">Collections</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a
+            href="/"
+            class="text-2xl block font-bold hover:text-[#FFFFFF66] transition-color transition-all"
+          >
+            Platforms
+          </a>
+          <ul class="flex flex-col gap-2 pt-3">
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Windows} alt="Windows" width={18} height={18} />
+                </span>
+                <span class="text-base">PC</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img
+                    src={Playstation}
+                    alt="Playstation"
+                    width={18}
+                    height={18}
+                  />
+                </span>
+                <span class="text-base">Playstation</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Xbox} alt="Xbox" width={18} height={18} />
+                </span>
+                <span class="text-base">Xbox one</span>
+              </a>
+            </li>
+            <li>
+              <a
+                class="text-lg font-normal flex items-center gap-2 group"
+                href="/"
+              >
+                <span class="p-2 bg-[#ffffff1a] text-white rounded-lg group-hover:bg-white group-hover:text-[#ffffff1a] transition-colors duration-300">
+                  <img src={Calendar} alt="calendar" width={18} height={18} />
+                </span>
+                <span class="text-base">Release calendar</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a
+            href="/"
+            class="text-2xl block font-bold hover:text-[#FFFFFF66] transition-color transition-all"
+          >
+            Genres
+          </a>
+          <ul class="flex flex-col gap-2 pt-3 pl-0.5">
+            <li>
+              <a class="text-lg font-normal flex items-center gap-2" href="/">
+                <img src={Action} alt="Action" width={32} height={32} />
+                <span class="text-base">Action</span>
+              </a>
+            </li>
+            <li>
+              <a class="text-lg font-normal flex items-center gap-2" href="/">
+                <img src={Strategy} alt="Strategy" width={32} height={32} />
+                <span class="text-base">Strategy</span>
+              </a>
+            </li>
+            <li>
+              <a class="text-lg font-normal flex items-center gap-2" href="/">
+                <img src={Rpg} alt="Rpg" width={32} height={32} />
+                <span class="text-base">RPG</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
 });
